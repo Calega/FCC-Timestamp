dde20af10a6b5d9ddefbbbd952d2922d11fb598e
lucascalegari <lcalegari17@gmail.com>
2019-01-19 16:29:01 -0800

    Making a few changes

--- server.js
+++ server.js
@@ -1,13 +1,7 @@
-// server.js
-// where your node app starts
+const express = require('express');
+const app = express();
+const cors = require('cors');
 
-// init project
-var express = require('express');
-var app = express();
-
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -18,25 +12,18 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+// Function to validate if the date instance is valid
+function isValidDate(d) {
+  return d instanceof Date && isFinite(d)
+}
 
 app.get("/api/timestamp/:date_string?/", function(req, res) {
   
-  var date = req.params.date_string ? new Date( req.params.date_string ) : new Date();
-  var isValidDate = isValidDate( date );
-  
-  // Function to validate if the date instance is valid
-  function isValidDate(d) {
-    return d instanceof Date && isFinite(d)
-  }
+  let date = req.params.date_string ? new Date( req.params.date_string ) : new Date();
 
   res.json({ 
-    "unix" : !isValidDate ? null : date.getTime(),
-    "utc" : !isValidDate ? "Invalid date" : date.toUTCString()
+    "unix" : !isValidDate( date ) ? null : date.getTime(),
+    "utc" : !isValidDate( date ) ? "Invalid date" : date.toUTCString()
   });
 });
 
